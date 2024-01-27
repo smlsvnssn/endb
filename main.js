ec1ce72f4d6d92ced471232193c11db8e17c4971
smlsvnssn <samuel@lhli.net>
2024-01-27 23:33:18 +0100

    set time to now

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import './style.css'
 
 console.log('Nothing to see here yet.')
+
+const getNow = () => new Date(Date.now()).toISOString().slice(0, -5)
+
+document.querySelector('.now').innerHTML = getNow()
