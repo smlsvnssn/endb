a2e411674a15e8b8018848db91d05113930a61fd
smlsvnssn <samuel@lhli.net>
2024-01-29 11:58:42 +0100

    bibliography + links

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ console.log('Nothing to see here yet.')
 
 const getNow = () => new Date(Date.now()).toISOString().slice(0, -5)
 
-document.querySelector('.now').innerHTML = getNow()
+try {
+    document.querySelector('.now').innerHTML = getNow()
+} catch (error) {
+    // It's alright
+}
